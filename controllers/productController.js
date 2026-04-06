b72a258e4255c4ef53ba3ef665a47c7ca0909aec
LeonMcCoubrey <lmccoubrey@udesa.edu.ar>
2026-04-06 17:46:43 -0300

    data.js hecho

--- controllers/productController.js
+++
@@ -1,6 +0,0 @@
-const data = 
-productos: [ 
-    {
-        id: 1,
-        name: Fiat Cronos
-    }
--- db/data.js
+++ db/data.js
@@ -2,24 +2,171 @@ let data = {
     productos: [
         {
             id: 1,
-            name: "Producto 1",
+            name: "Fiat Cronos",
+            precio: 20000,
+            kilometros: 50000,
             description: "Descripción del producto 1"
+        },
+        {
+            id: 2,
+            name: "Volkswagen Gol",
+            precio: 30000,
+            kilometros: 30000,
+            description: "Descripción del producto 2"
+        },
+        {
+            id: 3,
+            name: "Renault Sandero",
+            precio: 25000,
+            kilometros: 40000,
+            description: "Descripción del producto 3"
+        },
+        {
+            id: 4,
+            name: "Chevrolet Onix",
+            precio: 35000,
+            kilometros: 20000,
+            description: "Descripción del producto 4"
+        },
+        {
+            id: 5,
+            name: "Ford Fiesta",
+            precio: 15000,
+            kilometros: 60000,
+            description: "Descripción del producto 5"
+        },
+        {
+            id: 6,
+            name: "Toyota Corolla",
+            precio: 40000,
+            kilometros: 10000,
+            description: "Descripción del producto 6"
+        },
+        {
+            id: 7,
+            name: "Honda Civic",
+            precio: 45000,
+            kilometros: 8000,
+            description: "Descripción del producto 7"
+        },
+        {
+            id: 8,
+            name: "Nissan Sentra",
+            precio: 30000,
+            kilometros: 40000,
+            description: "Descripción del producto 8"
+        },
+        {
+            id: 9,
+            name: "Hyundai Elantra",
+            precio: 35000,
+            kilometros: 15000,
+            description: "Descripción del producto 9"
+        },
+        {
+            id: 10,
+            name: "Kia Rio",
+            precio: 25000,
+            kilometros: 50000,
+            description: "Descripción del producto 10"
         }
     ],
     comments: [
         {
             id: 1,
             comments: "Comentario 1"
+        },
+        {
+            id: 2,
+            comments: "Comentario 2"
+        },
+        {
+            id: 3,
+            comments: "Comentario 3"
+        },
+        {
+            id: 4,
+            comments: "Comentario 4"
+        },
+        {
+            id: 5,
+            comments: "Comentario 5"
+        },
+        {
+            id: 6,
+            comments: "Comentario 6"
+        },
+        {
+            id: 7,
+            comments: "Comentario 7"
+        },
+        {
+            id: 8,
+            comments: "Comentario 8"
+        },
+        {
+            id: 9,
+            comments: "Comentario 9"
+        },
+        {
+            id: 10,
+            comments: "Comentario 10"
         }
     ],
-    users:
+    users: [
     {
         id: 1,
         name: "Usuario 1",
         email: ""
-    }
+    },
+    {
+        id: 2,
+        name:"Usuario 2",
+        email: "",
+    },
+    {
+        id: 3,
+        name: "Usuario 3",
+        email: "",
+    }, 
+    {
+    id: 4,
+    name: "Usuario 4",  
+    email: "",
+    },
+    {
+    id: 5,
+    name: "Usuario 5",
+    email: "",
+    },
+    {
+    id: 6,
+    name: "Usuario 6",
+    email: "",
+    },
+    {
+    id: 7,
+    name: "Usuario 7",
+    email: "",
+    },
+    {
+    id: 8,
+    name: "Usuario 8",
+    email: "",
+    },
+    {
+    id: 9,
+    name: "Usuario 9",
+    email: "",
+    },
+    {
+    id: 10,
+    name: "Usuario 10",
+    email: "",
+    },
+]
+};
 
 
-}
 
 module.exports = data;
