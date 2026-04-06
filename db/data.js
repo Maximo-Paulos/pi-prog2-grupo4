2f203dfcf58a613568ec63e83a86aca8fa37d9ee
Maximo Paulos <mpaulosfrancisco@udesa.edu.ar>
2026-04-06 15:33:34 -0300

    bases

---
+++ db/data.js
@@ -0,0 +1,25 @@
+let data = {
+    productos: [
+        {
+            id: 1,
+            name: "Producto 1",
+            description: "Descripción del producto 1"
+        }
+    ],
+    comments: [
+        {
+            id: 1,
+            comments: "Comentario 1"
+        }
+    ],
+    users:
+    {
+        id: 1,
+        name: "Usuario 1",
+        email: ""
+    }
+
+
+}
+
+module.exports = data;
