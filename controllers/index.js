50fb9d79945d275892bdbc8bb874772181a78e06
Maximo Paulos <mpaulosfrancisco@udesa.edu.ar>
2026-04-06 15:40:28 -0300

    controllers

---
+++ controllers/index.js
@@ -0,0 +1,5 @@
+console.log("Controllers index.js cargado");
+
+const userController = require("./userController");
+const productController = require("./productController");
+const orderController = require("./orderController");
