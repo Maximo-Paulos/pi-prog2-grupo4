1033e2a4703b625a66b5337f798684023b5621e0
LeonMcCoubrey <lmccoubrey@udesa.edu.ar>
2026-04-20 17:44:28 -0300

    no se

--- routes/login.js
+++
@@ -1,8 +0,0 @@
-const router = express.Router();
-const express = require('express');
-const userController = require('../controllers/productController');
-
-router.get('/login', userController.login);
-router.get('/register', userController.register);
-
-module.exports = router;
