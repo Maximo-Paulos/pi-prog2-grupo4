85939651a5cffbd5819978b1a8a2950b1657f1ab
Maximo Paulos <mpaulosfrancisco@udesa.edu.ar>
2026-04-22 15:50:22 -0300

    tabla usuarios con un solo usuario

--- db/dato.sql
+++ db/dato.sql
@@ -1,2 +1,26 @@
 CREATE SCHEMA data;
+USE data;
 
+CREATE TABLE usuarios (
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    nombre VARCHAR(100) NOT NULL,
+    contrasenia VARCHAR(100) NOT NULL,
+    fotoPerfil VARCHAR(255),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deletedAt TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP
+);
+
+INSERT INTO usuarios (
+    email,
+    nombre,
+    contrasenia,
+    fotoPerfil
+)
+VALUES (
+    'leonmc@gmail.com',
+    'LeonMc',
+    'LeonMc123',
+    'fotoPerfil.jpg'
+);
