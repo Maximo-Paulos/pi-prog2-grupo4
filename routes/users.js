7e2ca40d00fdf7be0e22c45477c9467e8cf0a902
MilangrebU <mgrebenar@udesa.edu.ar>
2026-06-15 20:39:03 -0300

    emprolijo y muevo la validacion

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,23 @@ var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/userController');
 const {body} = require('express-validator');
+const db = require('../database/models');
 
 const validaciones = [
     body('email')
         .notEmpty().withMessage('El email es requerido')
         .bail()
-        .isEmail().withMessage('El email no es válido'),
+        .isEmail().withMessage('El email no es válido')
+        .bail()
+        // chequeo que el email no este ya registrado en la base
+        .custom(function (value) {
+            return db.User.findOne({ where: { email: value } })
+                .then(function (usuario) {
+                    if (usuario) {
+                        return Promise.reject('Ese email ya está registrado');
+                    }
+                });
+        }),
     body('nombre').notEmpty().withMessage('El nombre es requerido'),
     body('contrasenia').isLength({min: 3}).withMessage('La contraseña debe tener al menos 3 caracteres'),
 ];
