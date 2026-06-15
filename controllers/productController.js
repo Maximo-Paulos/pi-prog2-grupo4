6a322b2b61f800c40b7d165b14029f2d9cbc9fb8
LeonMcCoubrey <lmccoubrey@udesa.edu.ar>
2026-06-15 16:12:31 -0300

    creo la funcion de guardar producto en controlador

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,21 +1,35 @@
 const data = require('../localData/data');
-const {validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
 const db = require('../database/models');
 
 const controller = {
     productos: function (req, res) {
         res.render('index', { productos: data.productos, usuario: data.usuario });
     },
-     productAdd: function (req, res) {
+    productAdd: function (req, res) {
         res.render('product-add', { productos: data.productos, usuario: data.usuario })
     },
-     productEdit: function (req, res) {
+    productEdit: function (req, res) {
         res.render('product-edit', { productos: data.productos, usuario: data.usuario });
     },
-     product: function (req, res) {
+    product: function (req, res) {
         res.render('product', { productos: data.productos, usuario: data.usuario });
     },
-    
+    guardarProducto: function (req, res) {
+        const errors = validationResult(req);
+        if (errors.isEmpty()) {
+            let producto = {
+                id: data.productos.length + 1,
+                nombre: req.body.nombre,
+                descripcion: req.body.descripcion,
+                imagen: '/images/' + req.body.imagen
+            };
+            data.productos.push(producto);
+            res.redirect('/');
+        } else {
+            res.render('product-add', { errors: errors.array(), productos: data.productos, usuario: data.usuario });
+        }
+    },
     searchResults: function (req, res) {
         res.render('search-results', { productos: data.productos, usuario: data.usuario });
     },
@@ -28,7 +42,7 @@ const controller = {
                 producto = data.productos[i];
             }
         }
-        res.render('product', { producto: producto, usuario: data.usuario  });
+        res.render('product', { producto: producto, usuario: data.usuario });
     }
 };
 
